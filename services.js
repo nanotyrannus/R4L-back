0562e85b045bfc658c6dc56106050b8a858ef40b
Ryan Joseph Constantino <ryan.constantino93@gmail.com>
2016-02-26 17:40:53 -0800

    GEOJSON fix typo

--- services.js
+++ services.js
@@ -15,7 +15,7 @@ module.exports = {
     yield db.query(queryString);
 
     queryString = util.format(`
-      SELECT a.id, ST_AsGeoJSON(geom) AS geometry, (properties || jsonb_build_object('color', b.color)) AS properties
+      SELECT a.id, ST_AsGeoJSON(geom) AS geometry, (properties || jsonb_build_object('color', b.color)) AS properties, 'Feature' AS type
       FROM sites AS a FULL OUTER JOIN %s AS b ON a.id=b.id`, tableName);
     let result = yield db.query(queryString);
 
