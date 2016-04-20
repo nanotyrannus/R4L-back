f8126cb30d94ef84372022723dee00955aeb7a7d
Ryan Joseph Constantino <ryan.constantino93@gmail.com>
2016-04-19 23:51:10 -0700

    typo on services.init

--- services.js
+++ services.js
@@ -393,7 +393,7 @@ module.exports = {
                       last_name     text                          not null,
                       hash          text                          not null,
                       salt          text                          not null,
-                      weight        numeric(10, 5)                not null DEFAULT 1.00,
+                      weight        numeric(10, 5)                not null DEFAULT 1.00
                     )`)
 
                     yield db.query(`CREATE TABLE IF NOT EXISTS admins (
