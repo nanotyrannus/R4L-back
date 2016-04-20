c8d8a2d1f8f1e9839f2c71a77b5d293d49ef03d4
Ryan Joseph Constantino <ryan.constantino93@gmail.com>
2016-04-19 23:49:56 -0700

    Change QueryError to include offending queryString

--- shared/db.js
+++ shared/db.js
@@ -4,38 +4,39 @@ let postgres = require('pg');
 let pg       = require('co-pg')(postgres);
 let config = require("../config.json");
 
-var QueryError = function (errorCode, detail) {
+var QueryError = function (errorCode, detail, query) {
   this.name = "QueryError"
   this.message = detail || "Error processing database query"
+  this.query = query
   this.stack = (new Error()).stack
 }
 
 module.exports = {
   "query" : function* (queryString) {
     var timestamp, connectionResults, client, done, result
-    
+
     if (config.debug) {
       timestamp = new Date()
       console.log("db.query called: ", queryString, "\nTime:", timestamp)
     }
-    
+
     connectionResults = yield pg.connectPromise(config.aws)
     client = connectionResults[0]
     done = connectionResults[1]
-    
+
     try {
       result = yield client.queryPromise(queryString);
     } catch (e) {
       console.error(e)
-      throw new QueryError(e.code, e.detail)
+      throw new QueryError(e.code, e.detail, queryString)
     }
-    
+
     done();
-    
+
     if (config.debug) {
       console.log("Done: " + timestamp)
     }
-    
+
     return result
   }
 }
